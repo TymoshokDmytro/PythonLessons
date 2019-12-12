d9114587db37d56fa84345c548afc2f1cbf221ce
Tymoshok Dmytro <dtymochok@auchan.ua>
2019-12-12 09:44:12 +0200

    Done with complex number

--- lesson03/homework/03_complex.py
+++ lesson03/homework/03_complex.py
@@ -6,7 +6,7 @@ class ComplexNumber():
         self._img = img
 
     def __str__(self):
-        return 'ComplexNumber{' + self._real + ' + ' + self._img + 'i}'
+        return f'ComplexNumber({self._real} + {self._img}i)'
 
     def get_real(self):
         return self._real
@@ -32,9 +32,42 @@ class ComplexNumber():
             self.get_img() - other.get_img()
         )
 
-#TODO
     def __mul__(self, other):
         return ComplexNumber(
             (self.get_real() * other.get_real()) - (self.get_img() * other.get_img()),
-            (self.get_real() * other.get_real()) - (self.get_img() * other.get_img())
+            (self.get_img() * other.get_real()) + (self.get_real() * other.get_img())
         )
+
+    def __truediv__(self, other):
+        a = self.get_real()
+        b = self.get_img()
+        c = other.get_real()
+        d = other.get_img()
+
+        if any((c == 0, d == 0)):
+            raise ZeroDivisionError('Division by zero found: ' +
+                                    '\n ' + str(self) +
+                                    '\n ' + str(other))
+
+        real = ((a * c) + (b * d)) / ((c ** 2) + (d ** 2))
+        img = ((b * c) - (a * d)) / ((c ** 2) + (d ** 2))
+
+        return ComplexNumber(round(real, 4), round(img, 4))
+
+
+cmplx1 = ComplexNumber(3, 1)
+cmplx2 = ComplexNumber(5, -2)
+print(f'cmplx1={cmplx1}')
+print(f'cmplx2={cmplx2}')
+
+cmplx_res = cmplx1 + cmplx2
+print(f'Addition: {cmplx_res}')
+
+cmplx_res = cmplx1 - cmplx2
+print(f'Subtraction: {cmplx_res}')
+
+cmplx_res = cmplx1 * cmplx2
+print(f'Multiplication: {cmplx_res}')
+
+cmplx_res = cmplx1 / cmplx2
+print(f'Deletion: {cmplx_res}')

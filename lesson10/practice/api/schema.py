adfda52ed3ecb621211dadbf9a7148d1dfd2c0c9
TymoshokDmytro <tymoshok.dmytro@gmail.com>
2020-01-29 21:21:31 +0200

    Fix lesson 10 practice

--- lesson10/practice/api/schema.py
+++ lesson10/practice/api/schema.py
@@ -17,13 +17,13 @@ class TagSchema(Schema):
 
 class AuthorSchema(Schema):
     id = fields.String()
-    fullname = fields.String(max_length=64, required=True)
-    posts_count = fields.Int(min_value=0)
-
-    @validates('fullname')
-    def val_fullname(self, value):
-        if len(value) > 64:
-            raise ValidationError('Fullname must be < 64 length')
+    # fullname = fields.String(max_length=64, required=True)
+    # posts_count = fields.Int(min_value=0)
+    #
+    # @validates('fullname')
+    # def val_fullname(self, value):
+    #     if len(value) > 64:
+    #         raise ValidationError('Fullname must be < 64 length')
 
 
 class PostSchema(Schema):
@@ -31,6 +31,7 @@ class PostSchema(Schema):
     title = fields.String(max_length=64, required=True)
     description = fields.String(max_length=8192, required=True)
     creation_date = fields.DateTime(default=datetime.datetime.now())
-    author = fields.Nested(AuthorSchema)
+    #author = fields.Nested(AuthorSchema)
     views = fields.Int(min_value=0, default=0)
-    tags = fields.List(fields.Nested(TagSchema, dump_only=True))
+    author = fields.String()
+    tags = fields.List(fields.String(), dump_only=True)

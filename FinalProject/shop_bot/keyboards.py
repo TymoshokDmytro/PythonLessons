ad4a1146565d103f525eecca1a931658509d9c24
UKR0018794 <dtymochok@auchan.ua>
2020-02-28 13:14:05 +0200

    add old archive history

--- FinalProject/shop_bot/keyboards.py
+++ FinalProject/shop_bot/keyboards.py
@@ -2,5 +2,6 @@ START_KB = {
     'categories': 'Категории',
     'cart': 'Корзина',
     'promo': 'Акционные товары',
+    'archive': 'История заказов(callback)',
     'personal': 'Личный кабинет',
 }
